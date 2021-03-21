12b466f88a3f1ee7413320b61f3ea65f71106bd5
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2021-03-21 22:08:02 +0100

    comments section

--- schemas/schema.js
+++ schemas/schema.js
@@ -9,23 +9,9 @@ import blockContent from "./blockContent";
 import category from "./category";
 import post from "./post";
 import author from "./author";
-import comments from "./comments";
+import comment from "./comment";
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
   name: "default",
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    post,
-    author,
-    category,
-    comments,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-  ]),
+  types: schemaTypes.concat([post, author, category, comment, blockContent]),
 });
